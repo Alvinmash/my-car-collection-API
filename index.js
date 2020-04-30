3498aae43710de5e5caf2f894ef43a90107f72ad
Alvin Mwaura <alvinmash4@gmail.com>
2020-04-30 16:35:29 -0400

    commit on part-one-answer

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const carData = require('./cars')
+
+const app = express()
+
+app.set('view engine', 'pug')
+app.use(express.static('public'))
+
+// app.get('/cars', (_request, response) => {
+//   return response.send(carData)
+// })
+
+app.get('/cars', (_request, response) => {
+  return response.render('index', { carData })
+})
+
+
+
+app.all('*', (_request, response) => {
+  return response.status(404).send('sorry! page not found')
+})
+
+app.listen(7825, () => {
+  // eslint-disable-next-line no-console
+  console.log('guaranteed ! the port is listening on  7825')
+})
